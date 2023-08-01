4e42f0c407c453e7565414e0505f82c157cc6978
Muchovsky <muchovsky.garmory@gmail.com>
2023-08-01 23:42:00 +0200

    changes in item, item panel mock

--- Assets/Scripts/UI/Item.cs
+++ Assets/Scripts/UI/Item.cs
@@ -9,12 +9,22 @@ public class Item : MonoBehaviour
     [SerializeField] Image badgeImage;
     [SerializeField] Image glowImage;
     [SerializeField] TextMeshProUGUI description;
+    [SerializeField] TextMeshProUGUI number;
 
     [Inject] ItemSpriteList spriteList;
-    public void Init(CategoryType type, string descriptuon, bool isSpecial)
+    public void Init(int number, CategoryType type, string descriptuon, bool isSpecial)
     {
+        this.number.text = number.ToString();
         badgeImage.sprite = spriteList.GetByType(type).Sprite;
         description.text = descriptuon;
         glowImage.gameObject.SetActive(isSpecial);
+    }
+
+    public void Init(int number, DataItem dataitem)
+    {
+        this.number.text = number.ToString();
+        badgeImage.sprite = spriteList.GetByType(dataitem.Category).Sprite;
+        description.text = dataitem.Description;
+        glowImage.gameObject.SetActive(dataitem.Special);
     }
 }
--- Assets/Scripts/UI/ItemsPanel.cs
+++ Assets/Scripts/UI/ItemsPanel.cs
@@ -5,14 +5,14 @@ public class ItemsPanel : MonoBehaviour
 {
     [SerializeField] Item itemPrefab;
     List<Item> items;
-    public void Init(List<Item> items)
+    public void Init(List<DataItem> items)
     {
         CreateAttributes(items);
     }
-
-    void CreateAttributes(List<Item> items)
+    void CreateAttributes(List<DataItem> items)
     {
         PopulateList(items.Count);
+        InitItems(items);
     }
     void PopulateList(int count)
     {
@@ -20,6 +20,13 @@ public class ItemsPanel : MonoBehaviour
         {
             var prefab = Instantiate(itemPrefab, transform);
             items.Add(prefab);
+        }
+    }
+    void InitItems(List<DataItem> dataItems)
+    {
+        foreach (var item in items)
+        {
+            item.Init(0, dataItems[0]); //TODO
         }
     }
 }
