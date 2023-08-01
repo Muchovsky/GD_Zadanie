1278ecf420e4fbd43bdaa09583d29e0c98bcef5a
Muchovsky <muchovsky.garmory@gmail.com>
2023-08-01 23:27:43 +0200

    added Item Script, ItemSpriteList

--- Assets/Scripts/UI/Item.cs
+++ Assets/Scripts/UI/Item.cs
@@ -1,12 +1,20 @@
-using PlasticGui.WorkspaceWindow.Diff;
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+using static DataItem;
 
 public class Item : MonoBehaviour
 {
-    public void Init(ChangeCategoryType type, string descriptuon, bool isSpecial)
-    {
+    [SerializeField] Image badgeImage;
+    [SerializeField] Image glowImage;
+    [SerializeField] TextMeshProUGUI description;
 
+    [Inject] ItemSpriteList spriteList;
+    public void Init(CategoryType type, string descriptuon, bool isSpecial)
+    {
+        badgeImage.sprite = spriteList.GetByType(type).Sprite;
+        description.text = descriptuon;
+        glowImage.gameObject.SetActive(isSpecial);
     }
 }
